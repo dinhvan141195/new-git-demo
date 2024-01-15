7a315e12b2041b1f1ff2cbb29d700641913bf216
dinhvan141195 <dinhvan141195@gmail.com>
2024-01-15 19:25:05 +0700

    a

---
+++ test.js
@@ -0,0 +1,15 @@
+console.log("Nguyen Cao Dinh Van");
+let a = 2
+let b = 7
+
+while(a < b) {
+    console.log("Gia tri cua a la: %s", a);
+    a += 1;
+}
+
+if(b % 2 == 0) {
+    console.log("%s la so chan", b);
+} else {
+    console.log("%s la so le", b);
+}
+
